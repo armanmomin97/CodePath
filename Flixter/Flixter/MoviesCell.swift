610248d1577f4ebabc7dfceba928df6c1c73783b
Arman Momin <Arman1@armans-mbp-2.lan>
2019-02-10 13:02:01 -0600

    first commit

---
+++ Flixter/Flixter/MoviesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesCell.swift
+//  Flixter
+//
+//  Created by Arman Momin on 2/5/19.
+//  Copyright © 2019 armanmomin. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TipCalc/TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Arman Momin on 2/2/19.
+//  Copyright © 2019 armanmomin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
